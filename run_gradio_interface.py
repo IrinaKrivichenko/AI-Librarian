9d98f038013f2327b8ede75fb3274df23ffe7d2a
IrinaKrivichenko <38142046+IrinaKrivichenko@users.noreply.github.com>
2025-02-19 12:37:25 +0300

    add gradio

---
+++ run_gradio_interface.py
@@ -0,0 +1,11 @@
+import gradio as gr
+from RAG.RAG import RAG
+
+list_of_path_to_data = [ r"./data/database_docx/"]
+rag = RAG(list_of_path_to_data)
+
+def chatbot_function(input_text, _):
+       response = rag.get_answer(input_text)
+       return response
+
+gr.ChatInterface(chatbot_function).launch()
